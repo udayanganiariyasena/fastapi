bff108054749013086a5013a11695a547bb4b0e0
Udayangani Ariyasena <145590621+udayanganiariyasena@users.noreply.github.com>
2025-03-31 22:58:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = FastAPI()
 class Question(BaseModel):
     question: str
 
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
 @app.post("/ask")
 async def ask_hr(question: Question):
     answer = (question.question)
